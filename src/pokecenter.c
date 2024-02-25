cfd715d78c228d1ed6dc576c61011eb32a87532a
MenkoyS <yann.hobbe@supinfo.com>
2024-02-25 14:04:52 +0100

    feature(pokecenter) : back related , supémons now can be healed by the pokecenter

--- src/pokecenter.c
+++ src/pokecenter.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include "../include/player.h"
+
 void ask_where_to_go();
 
 void pokecenter() {
+    Player player;
     int choice;
     write(1, "+----------------------------+\n" , 32);
     write(1, "|What do you want to do?     |\n", 32);
@@ -14,7 +17,15 @@ void pokecenter() {
     scanf("%d", &choice);
     printf("You chose %d\n", choice);
     if (choice == 1) {
+        (&player)->supemons[0]->hp = (&player)->supemons[0]->max_hp;
+        (&player)->supemons[1]->hp = (&player)->supemons[1]->max_hp;
+        (&player)->supemons[2]->hp = (&player)->supemons[2]->max_hp;
+        (&player)->supemons[3]->hp = (&player)->supemons[3]->max_hp;
+        (&player)->supemons[4]->hp = (&player)->supemons[4]->max_hp;
+        (&player)->supemons[5]->hp = (&player)->supemons[5]->max_hp;
         printf("You healed your supemons\n");
+        printf("Supémons team hp : %d %d %d %d %d %d\n", (&player)->supemons[0]->hp, (&player)->supemons[1]->hp, (&player)->supemons[2]->hp, (&player)->supemons[3]->hp, (&player)->supemons[4]->hp, (&player)->supemons[5]->hp);
+        ask_where_to_go();
     }
     else if (choice == 2) {
         ask_where_to_go();
