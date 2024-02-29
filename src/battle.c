8781c22b38d28822f156055f4fd9a296dcd4f48b
MenkoyS <yann.hobbe@supinfo.com>
2024-02-29 01:30:10 +0100

    feature(fight) : advancements, now player can attack, change stats, win or loose

--- src/battle.c
+++ src/battle.c
@@ -4,19 +4,77 @@
 #include <string.h>
 #include <time.h>
 
-#include "../include/supemon.h"
+#include "../include/player.h"
 
 void ask_where_to_go();
 Supemon *getSupemonByID(int id);
 
-void fight() {
-    getSupemonByID(1), getSupemonByID(2), getSupemonByID(3);
-    char enemyEva[1], enemyAcc[1], enemyDef[1], enemyHP[1], enemyMaxHP[1], enemyLevel[1], enemyAtk[1];
+void displaySupemonStats(Player *ptrPlayer, Supemon *enemy);
 
-    srand(time(NULL));
-    int random = rand() % 3 + 1;
-    Supemon *enemy = getSupemonByID(random);
-    write(1, "WILD SUPEMON (", 14);
+void fight(Player *ptrPlayer) {
+    int choiceAttack, choice4; 
+    Supemon *enemy = NULL;
+
+    do {
+        if (enemy == NULL) {
+            srand(time(NULL));
+            int random = rand() % 3 + 1;
+            enemy = getSupemonByID(random);
+            displaySupemonStats(ptrPlayer, enemy);
+        }
+
+        write(1, "+----------------------------+\n" , 32);
+        write(1, "|What will you do?           |\n", 32);
+        write(1, "|    1. Move                 |\n", 32);
+        write(1, "|    2. Change Supemon       |\n", 32);
+        write(1, "|    3. Use item             |\n", 32);
+        write(1, "|    4. Capture              |\n", 32);
+        write(1, "|    5. Run away             |\n", 32);
+        write(1, "+----------------------------+\n" , 32);
+        write(1, "1, 2, 3, 4 or 5: ", 18);
+        scanf("%d", &choice4);
+
+        if (choice4 == 1) {
+            write(1, "\n1 - Scratch\n", 14);
+            write(1, "2 - Grawl\n", 11);
+            write(1, "3 - Cancel\n", 12);  
+            write(1, "1, 2 or 3 : ", 13);
+
+            scanf("%d", &choiceAttack);
+
+            if (choiceAttack == 1) {
+                write(1, "You used Scratch\n", 18);
+                enemy->hp -= 3;
+                ptrPlayer->supemons[0]->hp -= enemy->attack;
+                displaySupemonStats(ptrPlayer, enemy);
+            } else if (choiceAttack == 2) {
+                write(1, "You used Grawl\n", 16);
+                ptrPlayer->supemons[0]->attack += 1;
+                displaySupemonStats(ptrPlayer, enemy);
+            } else if (choiceAttack == 3) {
+                write(1, "You canceled\n", 14);
+            } else {
+                write(1, "Invalid choice\n", 16);
+            }
+        } else if (choice4 == 2) {
+            write(1, "You changed Supemon\n", 21);
+        } else if (choice4 == 3) {
+            write(1, "You used item\n", 14);
+        } else if (choice4 == 4) {
+            write(1, "You captured\n", 14);
+        } else if (choice4 == 5) {
+            write(1, "You ran away\n", 14);
+        } else {
+            write(1, "Invalid choice\n", 16);
+        }
+    } while (choice4 != 5 || enemy->hp <= 0 || ptrPlayer->supemons[0]->hp <= 0);
+}
+
+void displaySupemonStats(Player *ptrPlayer, Supemon *enemy) {
+    char enemyEva[100], enemyAcc[100], enemyDef[100], enemyHP[100], enemyMaxHP[100], enemyLevel[100], enemyAtk[100];
+    char hp[100], max_hp[100], level[100], atk[100], def[100], acc[100], eva[100];
+
+    write(1, "\nWILD SUPEMON (", 15);
     write(1, enemy->name, strlen(enemy->name));
     write(1, ")\n-----------------------\n", 26);
     write(1, "HP: ", 4);
@@ -41,44 +99,42 @@ void fight() {
     sprintf(enemyEva, "%d", enemy->evasion);
     write(1, enemyEva, strlen(enemyEva));
     write(1, "\n-----------------------\n", 25);
-    // can't work on that right now because the supémon stats are not initialized
-    write(1, "SUPMANDER (Benjamin) \n", 22);
-    write(1, "HP: 10/10       ", 17);
-    write(1, "Lvl: 1\n", 7);
-    write(1, "Atk: 1          ", 17);
-    write(1, "Def: 1\n", 8);
-    write(1, "Acc: 2          ", 17);
-    write(1, "Eva: 1\n", 8);
-    write(1, "-----------------------\n\n", 25);
-    write(1, "+----------------------------+\n" , 32);
-    write(1, "|What will you do?           |\n", 32);
-    write(1, "|    1. Move                 |\n", 32);
-    write(1, "|    2. Change Supemon       |\n", 32);
-    write(1, "|    3. Use item             |\n", 32);
-    write(1, "|    4. Capture              |\n", 32);
-    write(1, "|    5. Run away             |\n", 32);
-    write(1, "+----------------------------+\n" , 32);
-    write(1, "1, 2, 3, 4 or 5: ", 18);
-    int choice3;
-    scanf("%d", &choice3);
-    if (choice3 == 1) {
-        write(1, "\n1 - Scratch\n", 14);
-        write(1, "2 - Grawl\n", 11);
-        write(1, "3 - Cancel\n", 12);  
-        write(1, "1, 2 or 3 : ", 13);
-        int choiceAttack;
-        scanf("%d", &choiceAttack);        
-        printf("You used %d\n", choiceAttack);
-    } else if (choice3 == 2) {
-        write(1, "You changed Supemon\n", 21);
-    } else if (choice3 == 3) {
-        write(1, "You used an item\n", 17);
-    } else if (choice3 == 4) {
-        write(1, "You tried to capture the Supemon\n", 33);
-    } else if (choice3 == 5) {
-        write(1, "You ran away\n", 14);
+
+    write(1, ptrPlayer->supemons[0]->name, strlen(ptrPlayer->supemons[0]->name));
+    write(1, " (", 2);
+    write(1, ptrPlayer->name, strlen(ptrPlayer->name));
+    write(1, ")\n", 2);
+    write(1, "HP: ", 4);
+    sprintf(hp, "%d", ptrPlayer->supemons[0]->hp);
+    write(1, hp, strlen(hp));
+    write(1, "/", 1);
+    sprintf(max_hp, "%d", ptrPlayer->supemons[0]->max_hp);
+    write(1, max_hp, strlen(max_hp));
+    sprintf(level, "%d", ptrPlayer->supemons[0]->level);
+    write(1, "       Lvl: ", 12);
+    write(1, level, strlen(level));
+    write(1, "\nAtk: ", 6);
+    sprintf(atk, "%d", ptrPlayer->supemons[0]->attack);
+    write(1, atk, strlen(atk));
+    write(1, "          Def: ", 15);
+    sprintf(def, "%d", ptrPlayer->supemons[0]->defense);
+    write(1, def, strlen(def));
+    write(1, "\nAcc: ", 6);
+    sprintf(acc, "%d", ptrPlayer->supemons[0]->accuracy);
+    write(1, acc, strlen(acc));
+    write(1, "          Eva: ", 15);
+    sprintf(eva, "%d", ptrPlayer->supemons[0]->evasion);
+    write(1, eva, strlen(eva));
+    write(1, "\n-----------------------\n\n", 26);
+
+
+    if (ptrPlayer->supemons[0]->hp <= 0) {
+        write(1, "\nYour Supemon fainted\n", 22);
         ask_where_to_go();
-    } else {
-        write(1, "Invalid choice\n", 16);
+    }
+    else if (enemy->hp <= 0) {
+        write(1, "\nYou defeated the enemy\n", 24);
+        ask_where_to_go();
+
     }
 }
