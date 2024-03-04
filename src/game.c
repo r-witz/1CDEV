f8a752a877cfba8bd2eb3dadf170dafec9aea250
MenkoyS <yann.hobbe@supinfo.com>
2024-03-04 14:50:05 +0100

    feature(ask_name) : ask_name totally working

--- src/game.c
+++ src/game.c
@@ -1,31 +1,49 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 
 #include "../include/player.h"
 Supemon *getSupemonByID(int id);
 
 #define NAME_MAX_LENGTH 50
+#define MAX_NAME_LENGTH 12
 
 void ask_name(Player *ptrPlayer) {
-    char name[NAME_MAX_LENGTH];
+    char buffer[NAME_MAX_LENGTH];
+    char name[MAX_NAME_LENGTH + 1];
+
+    int isValid = 0;
+    size_t nameLength;
 
     do {
         write(1, "What's your name? ", 18);
-        fgets(name, sizeof(name), stdin);
-        name[strcspn(name, "\n")] = '\0';
+        fgets(buffer, sizeof(buffer), stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+        strncpy(name, buffer, sizeof(name));
 
-        if (strlen(name) == 0) {
-            write(1, "Please enter a valid name.\n", 27);
-            continue;
+        nameLength = strlen(name);
+
+        for (size_t i = 0; i < nameLength; ++i) {
+            if (!isalpha(name[i]) || nameLength > MAX_NAME_LENGTH) {
+                isValid = 0;
+                break;
+            }
+            isValid = 1;
         }
 
-        ptrPlayer->name = (char*)malloc(strlen(name) + 1);
-        strcpy(ptrPlayer->name, name);
-    } while (strlen(name) == 0);
-    
-    write(1, "Welcome in Supemon World ! ", 28);
+        if (nameLength == 0 || !isValid) {
+            write(1, "Please enter a valid name (letters only, max 12 characters) : ", 63);
+        }
+
+    } while (nameLength == 0 || !isValid);
+
+    ptrPlayer->name = (char *)malloc(nameLength + 1);
+    strcpy(ptrPlayer->name, buffer);
+
+    write(1, "Welcome in Supemon World! ", 27);
     write(1, ptrPlayer->name, strlen(ptrPlayer->name));
-    write(1, "\n", 1); 
+    write(1, "\n", 1);
 }
+
